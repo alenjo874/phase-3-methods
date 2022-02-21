152b8d79cd3dda3384a3249c86a5e1acc96a5b75
Alen Jo <alenjo874@gmail.com>
2022-02-21 16:58:50 -0500

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+def my_method(param)
+    puts "Running my_method"
+    param + 1
+  end
+
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(str)
+    puts "Hello, #{str}!"
+end
+
+def greet_with_default(str = "programmer")
+    puts "Hello, #{str}!"
+end
+
+def add(num_1, num_2)
+     num_1 + num_2
+end
+
+def halve(num)
+    if num.class == Integer
+        num/2
+    else
+        nil
+    end    
+end
